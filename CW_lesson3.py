19ba8c3b15abbb9769434dd9da362498577e11d6
Alexandra Tetiorkina <atrobber@gmail.com>
2025-09-30 13:43:28 +0300

    Add files via upload

---
+++ CW_lesson3.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+img = np.zeros((500,400,3), np.uint8)
+# img[:] = 232, 161, 116
+# rgb = bgr
+
+# img[100:150, 200:250] = 232, 161, 116
+
+cv2.rectangle(img,(100, 100),(200, 200),(232, 161, 116),1)
+
+cv2.line(img, (100, 100), (200, 200), (232, 161, 116),1)
+
+print(img.shape)
+cv2.line(img, (0, img.shape[0] // 2), (img.shape[1], img.shape[0] // 2), (232, 161, 116),1)
+cv2.line(img, (img.shape[1] // 2, 0), (img.shape[1] // 2, img.shape[0]), (232, 161, 116),1)
+
+
+cv2.circle(img, (200, 200), 100, (232, 161, 116),-1)
+cv2.putText(img, 'Alexandra Tetiorkina', (35, 30), cv2.FONT_HERSHEY_DUPLEX, 1, (232, 161, 116), 2)
+
+
+
+cv2.imshow("image", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
